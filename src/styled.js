de81dfb0ed30f94add88e542e95a15a232f40982
Aneta Szynal <anetaszynal28@gmail.com>
2021-01-18 22:18:01 +0100

    feat(): add global styles

--- src/styled.js
+++ src/styled.js
@@ -1,7 +1,23 @@
-import styled from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
+import {LABEL_WIDTH} from './lib/consts'
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+  
+  *, ::before, ::after {
+    box-sizing: inherit;
+  }
+  
+  body{
+    margin: 0;
+    font-family: "Fira Sans", Arial, Helvetica, Verdana,sans-serif;
+  }
+`
 
 export const WidgetWrapper = styled.main`
   display: grid;
-  grid-template-columns: 70px 1fr;
+  grid-template-columns: ${LABEL_WIDTH}px 1fr;
   user-select: none;
 `;
