d6b4eb65f05f40b690b4358226bafbd25846ad85
Aneta Szynal <anetaszynal28@gmail.com>
2021-01-18 22:26:06 +0100

    feat(): add global styles

--- src/styled.js
+++ src/styled.js
@@ -1,23 +1,7 @@
-import styled, { createGlobalStyle } from 'styled-components'
-import {LABEL_WIDTH} from './lib/consts'
-
-export const GlobalStyle = createGlobalStyle`
-  html {
-    box-sizing: border-box;
-  }
-  
-  *, ::before, ::after {
-    box-sizing: inherit;
-  }
-  
-  body{
-    margin: 0;
-    font-family: "Fira Sans", Arial, Helvetica, Verdana,sans-serif;
-  }
-`
+import styled from 'styled-components'
 
 export const WidgetWrapper = styled.main`
   display: grid;
-  grid-template-columns: ${LABEL_WIDTH}px 1fr;
+  grid-template-columns: 70px 1fr;
   user-select: none;
 `;
